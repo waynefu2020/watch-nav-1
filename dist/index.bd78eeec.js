82d8ef64528ed025db55f499ba411cf775746c43
DDWayne <652507565@qq.com>
2021-07-21 10:00:27 +0800

    可以点击新增网站

--- dist/index.bd78eeec.js
+++ dist/index.bd78eeec.js
@@ -442,7 +442,24 @@ id) /*: string*/
 }
 
 },{}],"30Yv7":[function(require,module,exports) {
-
+$('.addButton').on('click', () => {
+    let url = window.prompt('请问你要添加的网址是？')
+    console.log(url)
+    if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
+    }
+    console.log(url);
+    const $siteList = $('.siteList')
+    const $lastLi = $siteList.find('li.last')
+    const $li = $(`<li>
+                <a href="${url}">
+                                    <div class="site">
+                                        <div class="logo">${url[0]}</div>
+                                        <div class="link">${url}</div>
+                                    </div>
+                                </a>
+                </li>`).insertBefore($lastLi)
+})
 },{}]},["1Ypau","30Yv7"], "30Yv7", "parcelRequire427e")
 
 //# sourceMappingURL=index.bd78eeec.js.map
