142e663766bebb5a6b3eeae7011e18c0d8a7e07c
DDWayne <652507565@qq.com>
2021-07-21 11:11:43 +0800

    自动展示网址首字母大写

--- dist/index.bd78eeec.js
+++ dist/index.bd78eeec.js
@@ -442,24 +442,52 @@ id) /*: string*/
 }
 
 },{}],"30Yv7":[function(require,module,exports) {
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+
+const hashMap = xObject || [
+    {logo: 'V', url: 'https://cn.vuejs.org'},
+    {logo: 'R', url: 'https://react.docschina.org'},
+    {logo: 'S', url: 'https://stackoverflow.com'},
+]
+const simplifyUrl = (url)=>{
+    return url.replace('https://','')
+        .replace('http://','')
+        .replace('www.','')
+        .replace('/','')
+}
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+
+    hashMap.forEach(node => {
+        const $li = $(`<li>
+                <a href="${node.url}">
+                    <div class="site">
+                       <div class="logo">${node.logo}</div>
+                       <div class="link">${simplifyUrl(node.url)}</div>
+                    </div>
+                </a>
+           </li>`).insertBefore($lastLi)
+    })
+}
+
+render()
+
 $('.addButton').on('click', () => {
     let url = window.prompt('请问你要添加的网址是？')
-    console.log(url)
     if (url.indexOf('http') !== 0) {
         url = 'https://' + url
     }
-    console.log(url);
-    const $siteList = $('.siteList')
-    const $lastLi = $siteList.find('li.last')
-    const $li = $(`<li>
-                <a href="${url}">
-                                    <div class="site">
-                                        <div class="logo">${url[0]}</div>
-                                        <div class="link">${url}</div>
-                                    </div>
-                                </a>
-                </li>`).insertBefore($lastLi)
+    hashMap.push({logo: simplifyUrl(url)[0].toUpperCase(), url: url})
+    render()
 })
+
+window.onbeforeunload = () => {
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x',string)
+}
 },{}]},["1Ypau","30Yv7"], "30Yv7", "parcelRequire427e")
 
 //# sourceMappingURL=index.bd78eeec.js.map
